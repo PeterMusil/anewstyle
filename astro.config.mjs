4fe1f61163978eae1d602db3872bbcd3bf5fe095
Lukáš Chylík <lukas.chylik@riganti.cz>
2024-11-20 07:55:22 +0100

    npm update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    allowImportingTsExtensions: true,
     site: 'https://anewstyle.cz/',
+    base: "/",
 });
